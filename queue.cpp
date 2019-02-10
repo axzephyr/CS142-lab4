78b01245b6d82f1e39f5f2484b535b893ef7b4f9
axzephyr <42173172+axzephyr@users.noreply.github.com>
2019-02-10 23:48:11 +0530

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -13,16 +13,22 @@ class quue
         top=NULL;
         end=NULL;
     }
+    
+    
     void enquue(int value)
     {
         l1.insert(value);
         end=l1.head;
     }
+    
+    
     void dequue()
     {
         l1.delet();
         top=l1.tail;
     }
+    
+    
     bool isEmpty()
     {
         if(top==NULL)
@@ -34,6 +40,8 @@ class quue
             return false;
         }
     }
+    
+    
     int size()
     {
         int i=1;
@@ -41,6 +49,13 @@ class quue
         return i;
     }
 
+
+    void topdisplay()
+    {
+        cout<<top->data<<endl;
+    }
+    
+    
     void display()
     {
         l1.display();
@@ -57,7 +72,8 @@ int main()
     q1.enquue(5);
     q1.enquue(54);
     q1.dequue();
-    if(q1.isEmpty()==1)
+    q1.topdisplay();
+    if(q1.isEmpty()==0)
     {
         cout<<" the queue is not empty "<<endl;
     }
